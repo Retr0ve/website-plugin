b74f1a59afd53e02b7574ec833da6a2b2fe44c86
Retr0ve <zhaodacheng@hotmail.com>
2022-07-22 11:34:17 +0800

    feat: support mustache partials

--- utils/generateHTMLs.js
+++ utils/generateHTMLs.js
@@ -26,11 +26,20 @@ function loadTemplate(){
   });
 }
 
-function renderTemplates(templates, data){
+function loadTemplatePartials(){
+  const partialFiles = klawSync('./components', {nodir: true});
+  const partialData = {};
+  partialFiles.forEach(file => {
+    partialData[path.basename(file.path, '.mustache')] = fs.readFileSync(file.path, 'utf8');
+  })
+  return partialData;
+}
+
+function renderTemplates(templates, data, partials){
   templates.forEach(template => {
     const distPath = path.join(path.resolve(config.distDir), path.relative(config.rootDir, template.path).replace('pages', ''), template.name + '.html');
     console.log(`Rendering ${distPath}`);
-    const output = Mustache.render(template.content, data)
+    const output = Mustache.render(template.content, data, partials);
     fs.mkdirsSync(path.dirname(distPath));
     fs.writeFileSync(distPath, output, 'utf8');
   });
@@ -39,5 +48,6 @@ function renderTemplates(templates, data){
 module.exports = function() {
   const template = loadTemplate();
   const data = loadData();
-  renderTemplates(template, data);
+  const partials = loadTemplatePartials();
+  renderTemplates(template, data, partials);
 }
