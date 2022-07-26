6cc028a426317b87e78c738ed6eb76dda33781ee
Retr0ve <zhaodacheng@hotmail.com>
2022-07-26 14:07:30 +0800

    feat: homepage sections

--- data/plugins.js
+++ data/plugins.js
@@ -1,11 +1,55 @@
 async function fetchPluginData() {
-  plugins =  await fetch('https://raw.githubusercontent.com/joplin/plugins/master/manifests.json').then(res => res.json());
-  // plugins =  await fetch('https://raw.staticdn.net/joplin/plugins/master/manifests.json').then(res => res.json());
+  // const plugins =  await fetch('https://raw.githubusercontent.com/joplin/plugins/master/manifests.json').then(res => res.json());
+  // const plugins =  await fetch('https://raw.staticdn.net/joplin/plugins/master/manifests.json').then(res => res.json());
+  const plugins =  await fetch('https://raw.fastgit.org/joplin/plugins/master/manifests.json').then(res => res.json());
 
-  return Object.values(plugins);
+  return plugins;
 }
 
-module.exports = function() {
-  const plugins = fetchPluginData((res) => {return res});
+async function fetchStatsData(){
+  // const stats = await fetch('https://raw.githubusercontent.com/joplin/plugins/master/stats.json').then(res => res.json());
+  // const stats = await fetch('https://raw.staticdn.net/joplin/plugins/master/stats.json').then(res => res.json());
+  const stats = await fetch('https://raw.fastgit.org/joplin/plugins/master/stats.json').then(res => res.json());
+
+  return stats;
+}
+
+async function getTrendingPlugins(plugins, stats, topn){
+  let result = [];
+  for (const plugin in stats) {
+    if (Object.prototype.hasOwnProperty.call(stats, plugin)) {
+      const versions = Object.values(stats[plugin]);
+      const downloadCount = versions[versions.length - 1].downloadCount;
+      const createdAt = new Date(versions[versions.length - 1].createdAt);
+      const popularity = downloadCount / (Date.now() - createdAt)
+      result.push({
+        id: plugin,
+        popularity: popularity
+      });
+    }
+  }
+
+  return result.sort((a, b) => b.popularity - a.popularity).slice(0, topn).map((item) => {
+    return {
+      id: item.id,
+      popularity: item.popularity,
+      ...plugins[item.id],
+    }
+  });
+}
+
+module.exports = async function() {
+  const rawPluginsData = await fetchPluginData();
+  const rawStats = await fetchStatsData();
+  const allPlugins = Object.values(rawPluginsData);
+  const plugins = {
+    raw: {
+      plugins: rawPluginsData,
+      stats: rawStats
+    },
+    all: allPlugins,
+    recommended: allPlugins.filter(plugin => plugin._recommended),
+    trending: await getTrendingPlugins(rawPluginsData, await fetchStatsData(), 3),
+  };
   return plugins;
 };
