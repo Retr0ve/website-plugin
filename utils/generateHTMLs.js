4218d2cf895bffec345bed158a19624eed0ca8af
Retr0ve <zhaodacheng@hotmail.com>
2022-07-26 09:50:43 +0800

    feat: detail page

--- utils/generateHTMLs.js
+++ utils/generateHTMLs.js
@@ -5,13 +5,13 @@ const path = require('node:path');
 const config = require('../config.js');
 
 
-function loadData(){
+async function loadData(){
   const dataFiles = klawSync('./data', {nodir: true});
   const data = {};
-  dataFiles.forEach(file => {
-    let dataItem = require(file.path);
-    data[path.basename(file.path, '.js')] = dataItem();
-  })
+  for (let index = 0; index < dataFiles.length; index++) {
+    let getData = require(dataFiles[index].path);
+    data[path.basename(dataFiles[index].path, '.js')] = await getData();
+  }
   return data;
 }
 
@@ -35,19 +35,43 @@ function loadTemplatePartials(){
   return partialData;
 }
 
-function renderTemplates(templates, data, partials){
+function renderTemplates(templates, globalData, partials, routes){
   templates.forEach(template => {
-    const distPath = path.join(path.resolve(config.distDir), path.relative(config.rootDir, template.path).replace('pages', ''), template.name + '.html');
-    console.log(`Rendering ${distPath}`);
-    const output = Mustache.render(template.content, data, partials);
-    fs.mkdirsSync(path.dirname(distPath));
-    fs.writeFileSync(distPath, output, 'utf8');
+    if (template.name.search(/\[*\]/g) !== -1) {
+      const pathToken = template.name.slice(1, parseInt(template.name.search(/\[*\]/g)));
+      console.log(`Rendering dynamic route ${pathToken}`);
+      routes[pathToken].forEach(key => {
+        const distPath = path.join(path.resolve(config.distDir), path.relative(config.rootDir, template.path).replace('pages', ''), key, 'index.html');
+        console.log(`- Rendering ${distPath}`);
+        const data = {
+          ...globalData,
+          path: key,
+          plugin: {
+            ...globalData.plugins.raw.plugins[key],
+            ...globalData.plugins.raw.stats[key][globalData.plugins.raw.plugins[key].version]
+          }
+        }
+        const output = Mustache.render(template.content, data, partials);
+        fs.mkdirsSync(path.dirname(distPath));
+        fs.writeFileSync(distPath, output, 'utf8');
+      });
+
+    } else {
+      const distPath = path.join(path.resolve(config.distDir), path.relative(config.rootDir, template.path).replace('pages', ''), template.name + '.html');
+      console.log(`Rendering ${distPath}`);
+      const output = Mustache.render(template.content, globalData, partials);
+      fs.mkdirsSync(path.dirname(distPath));
+      fs.writeFileSync(distPath, output, 'utf8');
+    }
   });
 }
 
-module.exports = function() {
+module.exports = async function() {
   const template = loadTemplate();
-  const data = loadData();
+  const globalData = await loadData();
   const partials = loadTemplatePartials();
-  renderTemplates(template, data, partials);
+  const routes = {
+    'pluginName': globalData.plugins.all.map(plugin => plugin.id),
+  };
+  renderTemplates(template, globalData, partials, routes);
 }
