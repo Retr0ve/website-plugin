a2b0e7afd26e5aeee3178592b910cfd8eaaefe40
Retr0ve <zhaodacheng@hotmail.com>
2022-08-04 16:46:09 +0800

    feat: config

--- config.js
+++ config.js
@@ -1,4 +1,21 @@
-module.exports = {
-  rootDir: './',
-  distDir: './site',
+const config = {
+  mode: 'dev',
+  dev: {
+    rootDir: './',
+    distDir: './site',
+    site: ''
+  },
+  demo: {
+    rootDir: './',
+    distDir: './site',
+    site: 'website-plugin'
+  },
+  prod: {
+    rootDir: './',
+    distDir: './site',
+    site: ''
+  },
+}
+module.exports = () => {
+  return config[config.mode];
 }
